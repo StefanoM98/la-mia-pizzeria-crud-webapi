40f1fa5930e8390bf218ae5b24d012bf04ea9fc0
Utente <Utente@PC-STEFANO>
2023-10-10 14:50:50 +0200

    Refactoring funzione Load Pizza e pagina di dettagli mostrata tramite un tag <a>

--- la-mia-pizzeria-static/wwwroot/js/site.js
+++ la-mia-pizzeria-static/wwwroot/js/site.js
@@ -3,14 +3,60 @@
 
 // Write your JavaScript code.
 
-loadPizze()
+loadPizze();
+document.getElementById("search").addEventListener('keyup_enter', searchPizza)
+
+function searchPizza() {
+    let searchString = document.getElementById('search').value
+    loadPizze(searchString);
+}
+
 function loadPizze(searchKey) {
     axios.get('https://localhost:7087/api/Pizzas/GetPizze', {
         params: {
             search: searchKey
         }
     }).then((resp) => {
-        console.log(resp);
+        console.log(resp)
+        let result = resp.data;
+
+        const pizzaTable = document.getElementById('pizza-table');
+        const noPizza = document.getElementById('no-pizze');
+       
+        if (result.length == 0) {
+            pizzaTable.classList.add('d-none');
+            noPizza.classList.remove('d-none');
+
+        } else {
+
+            document.getElementById('pizze').innerHTML = ""
+            
+            result.forEach(pizza => {
+                document.getElementById('pizze').innerHTML +=
+                    `
+                        <tr>
+                            <td>
+                                <a href="/Pizza/Dettagli/${pizza.id}"> ${pizza.id}</a>
+                            </td>
+                            <td class="name">
+                                ${pizza.name}
+                            </td>
+                            <td class="description">
+                                ${pizza.description}
+                            </td>
+                            <td class="price">
+                                ${pizza.price}
+                            </td>
+                        </tr>
+                    `
+            })
+
+        }
     })
+        .catch((resp) => {
+            alert(resp)
+        })
 }
+
+
 
