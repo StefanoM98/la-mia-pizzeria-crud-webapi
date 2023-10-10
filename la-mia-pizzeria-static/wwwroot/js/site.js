abb63a686ca0d9fcc3dfed0fcb99cc1b5b02492a
Utente <Utente@PC-STEFANO>
2023-10-10 13:03:00 +0200

    Chiamata axios tramite console.log andata a buon fine

--- la-mia-pizzeria-static/wwwroot/js/site.js
+++ la-mia-pizzeria-static/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+loadPizze()
+function loadPizze(searchKey) {
+    axios.get('https://localhost:7087/api/Pizzas/GetPizze', {
+        params: {
+            search: searchKey
+        }
+    }).then((resp) => {
+        console.log(resp);
+    })
+}
+
